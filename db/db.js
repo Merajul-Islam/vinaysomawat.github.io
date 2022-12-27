3f6a649e2cfbeb0f7bdc3fb52e05d0173bca2394
Merajul Islam <79507694+Merajul-Islam@users.noreply.github.com>
2022-12-27 16:10:03 +0600

    Update db.js

--- db/db.js
+++ db/db.js
@@ -29,201 +29,108 @@ export default {
 		},
 		{
 			title: "Backend",
-			skillName: "Nodejs, MongoDB",
+			skillName: "SEO and Content Strategy",
 			color: "2",
-			percentage: "40",
+			percentage: "75",
 		},
 		{
 			title: "Clouds",
-			skillName: "AWS(EC2, S3), Heroku, Netlify",
+			skillName: "Creative Writing",
 			color: "3",
-			percentage: "30",
-		},
-		{
-			title: "Design",
-			skillName: "HTML, Bootstrap, SCSS",
-			color: "4",
 			percentage: "70",
 		},
 		{
+			title: "Design",
+			skillName: "Editing",
+			color: "4",
+			percentage: "50",
+		},
+		{
 			title: "Version Control",
-			skillName: "GitHub, JIRA, Trello",
+			skillName: "Adaptibility",
 			color: "7",
 			percentage: "70",
 		},
 		{
 			title: "Tools",
-			skillName: "Postman, Chrome DevTools",
+			skillName: "Time Management",
 			color: "3",
-			percentage: "80",
+			percentage: "95",
 		},
 		{
 			title: "Saas products",
-			skillName: "CleverTap, FreshDesk",
+			skillName: "Problem Solving",
 			color: "5",
-			percentage: "50",
+			percentage: "60",
 		},
 		{
 			title: "Editor",
-			skillName: "VS Code",
+			skillName: "HTML, CSS",
 			color: "6",
-			percentage: "70",
+			percentage: "85",
 		},
 	],
 	projects: {
-		web: [
+		Content: [
 			{
-				projectName: "Programming Diaries",
-				image: "images/programmingdiaries.png",
+				projectName: "Agent Writing",
 				summary:
-					"Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
-				preview: "https://programmingdiaries.herokuapp.com/",
+					"Written technical and non-technical content, proofread, editing content, managed social media platforms.",
+				preview: "https://agentwriting.com/",
 				techStack: [
-					"Django",
-					"SQLite",
-					"Bootstrap",
-					"JavaScript",
-					"Heroku",
+					"Content Writing",
+					"Editor",
+					"Social Media Manager",
+					"Video Editor",
+					"Business Development",
 				],
 			},
-			{
-				projectName: "Find Your Bank",
+		],
+		Marketing: [
+						{
+				projectName: "Leading Edge",
 				image: "images/findyourbank.png",
 				summary:
-					"Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
-				preview: "https://clever-fermi-0d5d76.netlify.app",
-				techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+					"Worked as a social media manager, fulfilled tasks, created cover photo, cover video, managed clients, written social media posts, etc.",
+				preview: "https://leadingedge.com.bd/",
+				techStack: ["Social Media Marketing", "Video Editing", "Communications", "Content Writing"],
 			},
-			{
-				projectName: "Web Portfolio",
-				image: "images/portfolio.png",
-				summary:
-					"Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
-				preview: "https://github.com/vinaysomawat/Travographer-Portal",
-				techStack: ["HTML", "Bootstrap", "JavaScript"],
-			},
-			{
-				projectName: "Resume Builder",
-				image: "images/resume-builder.png",
-				summary:
-					"Browser based editor to build and download Resumes in a customizable templates.",
-				preview: "https://vinaysomawat.github.io/Resume-Builder",
-				techStack: ["HTML", "Bootstrap", "JavaScript"],
-			},
-		],
-		software: [
-			{
-				projectName: "Pizza Ordering ChatBot",
-				image: "images/pizzaorderchatbot.png",
-				summary:
-					"ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
-				preview:
-					"https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
-				techStack: ["Dailogflow", "Firebase"],
-			},
-			{
-				projectName: "WhatsApp-Bot",
-				image: "images/whatsappbot.jpg",
-				summary:
-					"Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
-				preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
-				techStack: ["Selenium", "Chrome Webdriver", "Python"],
-			},
-			{
-				projectName: "Bill Generator",
-				image: "images/billgenerator.png",
-				summary:
-					"GUI to transfer data to excel sheets and generate bills on the local shops.",
-				preview: "https://github.com/vinaysomawat/Bill-Generator",
-				techStack: ["Tkinter", "Openxlpy", "Python"],
-			},
-		],
-		android: [
-			{
-				projectName: "NITW-CSE",
-				image: "images/nitwcse.jpg",
-				summary:
-					"The Application display details of Department courses, reference books, research, publication and faculty profile.",
-				preview: "https://github.com/vinaysomawat/NITW-CSE",
-				techStack: ["JAVA", "XML", "Android"],
-			},
-			{
-				projectName: "CareerHigh-App",
-				image: "images/carrerhigh.png",
-				summary:
-					"The Application display the webpages of website careerhigh.in in android devices.",
-				preview: "https://github.com/vinaysomawat/CareerHigh-Android",
-				techStack: ["JAVA", "XML", "Android"],
-			},
-		],
-		freelance: [
-			{
-				projectName: "SnylloAir.com",
-				image: "images/snylloair.png",
-				summary:
-					"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
-				preview: "https://www.snylloair.com/",
-				techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
-			},
-			{
-				projectName: "Delivery+",
-				image: "images/AM-Logo-.png",
-				summary:
-					"Android Application to display website in android devices.",
-				preview:
-					"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
-				techStack: ["Android", "JAVA", "Play Store"],
-			},
+			
 		],
 	},
 	experience: [
 		{
-			title: "Biofourmis India Pvt. Ltd.",
-			duration: "April 2022 - Present",
-			subtitle: "Software Engineer",
+			title: "Agent Writing",
+			duration: "April 2021 - Present",
+			subtitle: "Web Content Writer",
 			details: [
-				"Working on the products in the healthcare/digital therapeutics domain."
+				"Working as a web content writer in different niches."
 			],
 			tags: [
-				"JavaScript",
-				"Angular",
-				"Bootstrap",
-				"Nodejs",
-				"Jenkins",
+				"Content Writer",
+				"Digital Marketer",
+				"On-Page SEO",
+				"Off-Page SEO",
 			],
 			icon: "heartbeat",
 		},
 		{
-			title: "Novopay Solutions Pvt. Ltd.",
-			duration: "June 2020 - April 2022",
-			subtitle: "Software Engineer",
+			title: "LEADING EDGE",
+			duration: "March 2020 - March 2021",
+			subtitle: "Social Media Manager, Content Writer",
 			details: [
-				"Implemented Aadhaar Enabled Payment services such as Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements, and Money transfer; completed more than 20 story points in each sprint.",
-				"Integrated QR Code and reduced the effective time by 50 percent to load money into wallet, Clevertap events to track user actions, Freshdesk ticketing service and chat-bot services. Worked on user onboarding, approval, and finance interfaces.",
-				"Co-ordinated closely with the product team, backend team, android team, and QA team to deliver the product builds before deadlines.",
+				"Managed clients came through social media, created scheduled posts.",
+				"Created engaging videos and flyers for social media posting",
+				"Written content to gain more traffic in the websites and social media platforms.",
 			],
 			tags: [
-				"JavaScript",
-				"Angular",
-				"React",
-				"Bootstrap",
-				"Nodejs",
-				"Jenkins",
+				"Social Media",
+				"Content Writing",
+				"Video Editing",
+				"Digital Marketing",
+				"SEO",
 			],
 			icon: "qrcode",
-		},
-		{
-			title: "ThinkPedia LLP",
-			duration: "May 2019 - June 2019",
-			subtitle: "SDE Intern",
-			details: [
-				"Worked as a full stack developer to support tech team.",
-				"Developed a customer Web Application from scratch for social media management.",
-			],
-			tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
-			icon: "group",
-		},
-	],
 	education: [
 		{
 			title: "Bachelors in Computer Science and Engineering",
